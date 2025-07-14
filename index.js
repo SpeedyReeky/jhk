b3b50788cb3e28aa28fa98a2378f7f73facc77ef
SpeedyReeky <kokaaomar.10311@gmail.com>
2025-07-14 09:40:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+let f= document.getElementById("Sun")
+f.style.color="yellowgreen"
+f.innerHTML ="hello"
+let fo=document.getElementsByClassName("atr")[0]
+  
+                                 fo.innerHTML ="Bye"
+                       fo.style.color="green"
+                       
+                       
+
+                       
+                  let fg=document.getElementsByTagName("p");alert("Number of <p> element(s):"+fg.length
+                  )    
+
+let d=document.body.style.backgroundColor=" rgba(236, 125, 52, 1)"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
